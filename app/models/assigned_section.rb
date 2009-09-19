6e7abcbc391130588632114647c2e2cf41955108
Marcus Vorwaller <marcus@vorwaller.net>
2009-09-19 14:25:09 -0700

    Remove articles

--- app/models/assigned_section.rb
+++
@@ -1,6 +0,0 @@
-class AssignedSection < ActiveRecord::Base
-  belongs_to :article
-  belongs_to :section
-  acts_as_list :scope => :section_id
-  validates_presence_of :article_id, :section_id
-end
--- app/models/comment.rb
+++
@@ -1,8 +0,0 @@
-class Comment < Content
-  validates_presence_of :user_id, :body
-  
-  belongs_to :article
-  
-  acts_as_nested_set
-  
-end
